b89faf3bf8d9f9a3dececa64861846932f1fa894
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2018-08-10 22:00:49 -0300

    Add coap

---
+++ coap.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io/ioutil"
+	"net"
+
+	coap "github.com/dustin/go-coap"
+)
+
+type coapHandler struct {
+}
+
+func (h *coapHandler) ServeCOAP(l *net.UDPConn, a *net.UDPAddr, req *coap.Message) *coap.Message {
+	if containsFile(req.PathString()) != nil {
+		fetchFile(req.PathString())
+	}
+
+	payload, err := ioutil.ReadFile(req.PathString())
+	if err != nil {
+		panic(err)
+	}
+
+	return &coap.Message{
+		Type:      coap.Acknowledgement,
+		MessageID: req.MessageID,
+		Token:     req.Token,
+		Code:      coap.Content,
+		Payload:   payload,
+	}
+}
